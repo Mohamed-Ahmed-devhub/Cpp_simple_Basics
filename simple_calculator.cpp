4606555a7b65e17da1e3156a60b1b5cfb13876ba
Mohamed_Ahmed <mohamed.ahmed.programer@gmail.com>
2026-01-27 09:17:18 +0200

    Add files via upload

---
+++ simple_calculator.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n1, n2, op;
+    cout << " type number one\n";
+    cin >> n1;
+    cout << " type number tow\n";
+    cin >> n2;
+    cout << " choosh operation number\n ";
+
+    cout << "[1] +\n";
+    cout << "[2] -\n";
+    cout << "[3] /\n";
+    cout << "[4] *\n";
+    cin >> op;
+
+    switch (op)
+    {
+    case 1:
+        cout << n1 << "+" << n2 << "=" << n1 + n2 << "\n";
+        break;
+
+    case 2:
+        cout << n1 << "-" << n2 << "=" << n1 - n2 << "\n";
+        break;
+
+    case 3:
+        cout << n1 << "/" << n2 << "=" << n1 / n2 << "\n";
+        break;
+
+    case 4:
+        cout << n1 << "*" << n2 << "=" << n1 * n2 << "\n";
+        break;
+
+    }
+
+    return 0;
+}
